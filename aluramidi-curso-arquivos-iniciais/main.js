cc38a0c08c41e383b35afe494ed56f62df044acd
matheuscalvaocria <134975238+matheuscalvaocria@users.noreply.github.com>
2023-09-25 19:37:44 -0300

    .

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,8 +1,7 @@
 // Som Pom--------------------------------------------------
 function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
-}
-
-const listaDeTeclas = document.querySelectorAll('.tecla');
-
+   
+ }
+ document.querySelector('.tecla_pom').onclick = tocaSomPom
 
