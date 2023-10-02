421113246732d8e84fc384bc1712a4c0732e78a3
matheuscalvaocria <134975238+matheuscalvaocria@users.noreply.github.com>
2023-10-02 19:27:25 -0300

    .

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
+// Pom---------------------------------------------------------
 function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
-   
  }
-
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
 
-
-
+//enquanto
+while () {
+    listaDeTeclas[0].onclick = tocaSomPom;
+}
