dcff27f79b5f3fade33e83cbdc6b7cf3aedbdaf0
matheuscalvaocria <134975238+matheuscalvaocria@users.noreply.github.com>
2023-10-02 19:36:53 -0300

    .

--- main.js
+++ main.js
@@ -9,10 +9,11 @@ let contador = 0;
 
 //enquanto
 while (contador < 9) {
-    listaDeTeclas[0].onclick = tocaSomPom;
+
+    listaDeTeclas[contador].onclick = tocaSomPom;
 
     contador = contador + 1;
 
     console.log(contador);
-    
+
 }
